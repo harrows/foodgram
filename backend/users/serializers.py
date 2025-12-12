e7b8173c3dc90dec09a299342b34567a8b5d1f4f
Дмитрий Машонский <harrows2012@gmail.com>
2025-12-13 02:01:26 +0300

    fix_Fix is_subscribed

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -19,9 +19,16 @@ class UserSerializer(serializers.ModelSerializer):
         )
 
     def get_is_subscribed(self, obj):
-        user = self.context.get('request').user
-        if user.is_anonymous:
+        request = self.context.get('request')
+        if not request or request.user.is_anonymous:
             return False
+
+        user = request.user
+
+        # пользователь не может быть подписан на самого себя
+        if user == obj:
+            return False
+
         return Follow.objects.filter(user=user, author=obj).exists()
 
 
